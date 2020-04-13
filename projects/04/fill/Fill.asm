406a2364e1ee2d0c3b5fc0d414bbd7d160debb86
IshiiDaisuke <isiidaisuke0926@yahoo.co.jp>
2020-04-13 09:41:19 +0900

    04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,55 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(INIT)
+    @SCREEN
+    D=A      // D <- SCREEN
+    @addr    // 塗りつぶしたいSCREEN上のワード
+    M=D      // SCREEN
+
+    // if(KBD > 0...pressed) goto PRESS
+    @KBD
+    D=M
+    @PRESS
+    D;JGT
+    @ELSE
+    0;JMP
+(PRESS) //color select
+    @color
+    M=-1     // color=-1 black
+    @ENDIF
+    0;JMP
+(ELSE) //color select
+    @color
+    M=0      // color=0 白
+(ENDIF)
+
+(LOOP)      
+    @color
+    D=M      // D <- color
+    @addr
+    A=M
+    M=D      // *addr <- color
+    @addr
+    M=M+1
+
+    @8192
+    D=A
+    @SCREEN
+    D=D+A
+    @addr
+    D=D-M  // D= 8192 + SCREEN - addr
+    // 塗り残しがあれば塗りつぶすまで続ける
+    @LOOP
+    D;JGT
+
+    // 全部塗り終わったら、最初に戻る
+    @INIT
+    0;JMP
+
+// このプログラムは永久に終了しない
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,22 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+//Initial
+  @R2
+  M=0 //R <- 0
+(LOOP)
+  @R1 //if(R1 <= 0) END
+  D=M
+  @END
+  D;JLE
+  @R0 //R2 <- R2+R0
+  D=M
+  @R2
+  M=D+M
+  @R1 //R1--
+  M=M-1
+  @LOOP // loop
+  0;JMP
+(END)
+  @END
+  0;JMP
